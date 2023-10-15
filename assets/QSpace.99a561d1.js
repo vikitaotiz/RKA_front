ce2b211920cf3f8703f484ed64d609da8936ca5f
vikitaotiz <victorotieno598@gmail.com>
2023-10-16 00:44:28 +0300

    rka4

---
+++ assets/QSpace.99a561d1.js
@@ -0,0 +1,1 @@
+import{h as e,bA as a}from"./index.db1ee41f.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
---
+++ assets/vuequery.d4dc1594.js
@@ -0,0 +1,1 @@
+import{V as r}from"./index.bf090e03.js";import"./index.db1ee41f.js";var o=({app:e})=>{e.use(r)};export{o as default};
